fbb139469ccd01fbea4c29536ac3a899685cc571
wangjunting <wangjunting@juxinli.com>
2018-04-09 17:47:46 +0800

    修改MergeTwoSortedLists解决方案

--- src/main/java/cn/wang/leetcode/MergeTwoSortedLists.java
+++ src/main/java/cn/wang/leetcode/MergeTwoSortedLists.java
@@ -11,30 +11,29 @@ package cn.wang.leetcode;
  */
 public class MergeTwoSortedLists {
 	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-		ListNode dummy = new ListNode(0);
-		ListNode  l3 = dummy;
-		while(l1 != null && l2 != null){
-			if(l1.val <= l2.val){
-				l3.next=l1;
-				l1=l1.next;
-			}
-			else{
-				l3.next=l2;
-				l2=l2.next;
-			}
-			l3=l3.next;
-		}
-		while(l1 != null){
-			l3.next = l1;
-			l1=l1.next;
-			l3=l3.next;
-		}
-		while(l2 !=null){
-			l3.next=l2;
-			l2=l2.next;
-			l3=l3.next;
-		}
-		l3.next=null;
-		return dummy.next;
-	}
+        ListNode dummy = new ListNode(0);
+        ListNode l3 = dummy;
+        while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                l3.next = l1;
+                l1 = l1.next;
+            }else{
+                l3.next = l2;
+                l2 = l2.next;
+            }
+            l3=l3.next;
+        }
+        while(l1 != null){
+            l3.next = l1;
+            l1=l1.next;
+            l3=l3.next;
+        }
+        while(l2 != null){
+            l3.next = l2;
+            l2 = l2.next;
+            l3=l3.next;
+        }
+        l3.next=null;
+        return dummy.next;
+    }
 }
